4836dc4f366f929f832297d428abcb072e3a35e8
xiaogao0804 <1941828283@qq.com>
2017-11-07 17:05:47 +0800

    列表组件

--- src/components/lists/list.vue
+++ src/components/lists/list.vue
@@ -2,18 +2,21 @@
     <div class='listWrap'> 
        <ul>
           <li v-for="item in items">
-            {{item.msg}}
+             <img :src="item.imgUrl" class="listImg"/>
           </li>
        </ul>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
+   import one from '../../assets/check.png'
+
    export default{
        name:'list',
        data(){
            return{
               items:[
+                  {imgUrl:one},
                   {msg:'ss'},
                   {msg:'ee'},
                   {msg:'tt'},
@@ -24,8 +27,27 @@
    }
 </script>
 
-<style>
+<style scope>
    .listWrap{
-      width:90%;
+      width:100%;
+   
    }
+   .listWrap ul{
+      width:96%;
+      margin:0.2rem auto;
+      -webkit-padding-start: 0;
+
+   }
+   .listWrap ul li{
+      width:100%;
+      list-style-type:none;
+      background-color:#fff;
+      margin-bottom:0.2rem;
+       padding:0.3rem 0;
+   }
+  .listWrap .listImg{
+      width:1.2rem;
+      height:1.5rem;
+      margin-left:0.3rem;
+  }
 </style>
