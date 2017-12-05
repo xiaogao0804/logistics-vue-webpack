5074c31f6590b49d998450ad8cd6786cd271bdae
Jarvan <liushileijarvan@gmail.com>
2017-12-05 16:17:12 +0800

    出货列表组件完成

--- src/components/lists/list.vue
+++ src/components/lists/list.vue
@@ -1,9 +1,20 @@
 <template>
-    <div class='listWrap'> 
+    <div class='listWrap'>
        <ul>
-          <li v-for="item in items">
+         <a href="www.baidu.com" v-for="item in items">
+           <li>
              <img :src="item.imgUrl" class="listImg"/>
-          </li>
+             <div class="listTxt">
+               <p>{{item.msgDate}}</p>
+               <p>{{item.msgTxt}} &nbsp;{{item.msgNum}}</p>
+             </div>
+             <p class="state">
+               {{item.msgState}}
+             </p>
+             <i class="fa fa-angle-right"></i>
+           </li>
+         </a>
+
        </ul>
     </div>
 </template>
@@ -16,12 +27,13 @@
        data(){
            return{
               items:[
-                  {imgUrl:one},
-                  {msg:'ss'},
-                  {msg:'ee'},
-                  {msg:'tt'},
-                  {msg:'rr'}
-              ]
+                {imgUrl:one,msgDate:'20170910001',msgTxt:'出货数量',msgNum:5,msgState:'完成'},
+                {imgUrl:one,msgDate:'20170910001',msgTxt:'出货数量',msgNum:5,msgState:'完成'},
+                {imgUrl:one,msgDate:'20170910001',msgTxt:'出货数量',msgNum:5,msgState:'完成'},
+                {imgUrl:one,msgDate:'20170910001',msgTxt:'出货数量',msgNum:5,msgState:'完成'},
+                {imgUrl:one,msgDate:'20170910001',msgTxt:'出货数量',msgNum:5,msgState:'完成'}
+              ],
+
            }
        }
    }
@@ -30,7 +42,10 @@
 <style scope>
    .listWrap{
       width:100%;
-   
+
+   }
+   a{
+     color:#000;
    }
    .listWrap ul{
       width:96%;
@@ -43,11 +58,33 @@
       list-style-type:none;
       background-color:#fff;
       margin-bottom:0.2rem;
-       padding:0.3rem 0;
+      padding:0.3rem 0;
+      position:relative;
    }
   .listWrap .listImg{
       width:1.2rem;
       height:1.5rem;
       margin-left:0.3rem;
+  }
+   .listWrap .fa-angle-right{
+     position:absolute;
+     right:10px;
+     top:50%;
+     transform: translateY(-50%);
+     font-size:0.64rem;
+   }
+  .listTxt{
+    position:absolute;
+    top:50%;
+    transform: translateY(-50%);
+    left:70px;
+    font-size:0.38rem;
+  }
+  .listWrap .state{
+    position:absolute;
+    right:80px;
+    top:30%;
+    transform: translateY(-50%);
+    font-size:0.64rem;
   }
 </style>
